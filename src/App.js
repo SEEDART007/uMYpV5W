46d723e4b365a7e8d8c3f1e6f7334fce5e0bef21
SEEDART007 <143023043+SEEDART007@users.noreply.github.com>
2024-03-17 13:23:16 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Design from './Design.css'
 import axios from 'axios'
+import fan from './fan.jpg'
 
 function App() {
   const[fetchedData,setFetchedData]=useState([])
@@ -22,7 +23,18 @@ const getData=async()=>{
   
 }
 
-const handleFan=()=>{
+const handleFan=async(e)=>{
+  setSpeedOfFan(e.target.value)
+  let data={
+    teamid:"uMYpV5W",
+    device:"fan",
+    value:speedOfFan+1,}
+
+  const res=await axios.post('https://kodessphere-api.vercel.app/devices',data)
+  if(speedOfFan>5){
+    alert("speed of fan should be less than")
+  }
+
 
 }
 
@@ -44,6 +56,7 @@ const handleBulb=async()=>{
     console.log("post",res)
     setIsBulbOn(false)
     console.log("bulb is on")
+    window.location.reload()
     
   }else{
     let data={
@@ -55,7 +68,7 @@ const handleBulb=async()=>{
     console.log("post",res)
     setIsBulbOn(true)
     console.log("bulb is off")
-    
+    window.location.reload()
   }
 
 }
@@ -64,29 +77,39 @@ const handleBulb=async()=>{
   <div>
     {
       fetchedData!=0?(fetchedData&&<div className='megabox'>
+        <div className='head'>
+        <h2>Simulation page for Bytes</h2>
+        <p>This page listens to the changes in the database and updates the UI in real time</p>
+        </div>
       <div className='superbox'>
       <div className='box1'>
         <h2>Fan</h2>
         <p>Control the speed of the fan</p>
-       <input value={fetchedData.fan||speedOfFan} type='number' onChange={e=>setSpeedOfFan(e.target.value)}></input>
+        <h1>☢</h1>
+        
+       <input placeholder='speed'  className='faninput' value={fetchedData.fan&&speedOfFan} type='number' onChange={handleFan}></input>
 
       </div>
       <div className='box2'>
       <h2>Bulb</h2>
-        <p>Control the speed of the fan</p>
-        <button onClick={handleBulb}>{fetchedData.bulb===0?"off":"on"}</button>
+        <p>Turn on or off the bulb</p>
+        <h1>💡</h1>
+        <button className='btn-fan' onClick={handleBulb}>{fetchedData.bulb===0?"off":"on"}</button>
 
       </div>
       <div className='box3'>
       <h2>LED</h2>
-        <p>Control the speed of the fan</p>
-       <input value={fetchedData.led} type='text' onChange={e=>setLedColor(e.target.value)}></input>
+        <p>Control the color of the led</p>
+        <h1>🚨</h1>
+       <input className='led-input' value={fetchedData.led&&LedColor} type='text' onChange={e=>setLedColor(e.target.value)}></input>
+
       </div>
       <div className='box4'>
       <h2>AC</h2>
-        <p>Control the speed of the fan</p>
-       <button onClick={handleFan}>{fetchedData.ac.state===0?"off":"on"}</button>
-       <input value={fetchedData.ac.temp} type='number' onChange={e=>setTempOfAc(e.target.value)}></input>
+        <p>Control state & temp of Ac</p>
+        <h1>❄️</h1>
+       <button className='ac-btn' onClick={handleFan}>{fetchedData.ac.state===0?"off":"on"}</button>
+       <input className='ac-input' value={fetchedData.ac.temp&&tempOfAc} type='number' onChange={e=>setTempOfAc(e.target.value)}></input>
       </div>
       </div>
     </div>):(<h1>loading</h1>)
